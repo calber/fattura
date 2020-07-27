70cc46127cea423526ad345a8be6845cccfe353e
Carlo Alberto Negri <carloalberto.negri@gmail.com>
2020-07-27 13:31:14 +0200

    Initial Commit

---
+++ fattura/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  fattura
+//
+//  Created by Alberto Negri on 27/7/20.
+//  Copyright © 2020 calber. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fatturaTests/fatturaTests.swift
@@ -0,0 +1,34 @@
+//
+//  fatturaTests.swift
+//  fatturaTests
+//
+//  Created by Alberto Negri on 27/7/20.
+//  Copyright © 2020 calber. All rights reserved.
+//
+
+import XCTest
+@testable import fattura
+
+class fatturaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
