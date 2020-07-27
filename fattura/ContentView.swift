fe392d963597cf67e6e1c07f8e97860cc18bc79d
Carlo Alberto Negri <carloalberto.negri@gmail.com>
2020-07-27 18:17:35 +0200

    initial

--- fattura/ContentView.swift
+++ fattura/ContentView.swift
@@ -7,11 +7,18 @@
 //
 
 import SwiftUI
+import XMLTools
+import UIKit
 
 struct ContentView: View {
+    let fattura = Reader(file: "IT07007590966_SGTK7.xml").read()
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text(fattura.committente.iva.IdCodice)
+            Text(fattura.committente.anagrafica.denominazione)
+            Text(fattura.prestatore.sede.indirizzo)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
@@ -20,4 +27,81 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class DocumentPickerViewController: UIDocumentPickerViewController {
+    private let onDismiss: () -> Void
+    private let onPick: (URL) -> ()
+
+    init(supportedTypes: [String], onPick: @escaping (URL) -> Void, onDismiss: @escaping () -> Void) {
+        self.onDismiss = onDismiss
+        self.onPick = onPick
+
+        super.init(documentTypes: supportedTypes, in: .open)
+
+        allowsMultipleSelection = false
+        delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DocumentPickerViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        onPick(urls.first!)
+    }
+
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        onDismiss()
+    }
+}
+
+class Reader {
+
+    var xml: XMLTools.Infoset?
+    
+    init(file: String)  {
+        let parser = XMLTools.Parser()
+
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(file)
+            do {
+                let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+                parser.options.trimWhitespaces = true
+                
+                xml = try parser.parse(string: text2)
+                xml?.namespaceContext.declare("ns0", uri: "http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2")
+                
+            }
+            catch {
+                debugPrint("\(error)")
+            }
+        }
+        
+    }
+    
+    func read() -> Fattura {
+        let builder = FatturaBuilder()
+        
+        if let c = xml?["FatturaElettronica","FatturaElettronicaHeader","CessionarioCommittente","DatiAnagrafici"] {
+            builder.committente(iva: c["IdFiscaleIVA","IdCodice"].text,
+                                paese: c["IdFiscaleIVA","IdPaese"].text,
+                                nome: c["Anagrafica","Denominazione"].text)
+        }
+        if let p = xml?["FatturaElettronica","FatturaElettronicaHeader","CedentePrestatore"] {
+            builder.prestatore(iva: p["DatiAnagrafici","IdFiscaleIVA","IdCodice"].text,
+                               paese: p["DatiAnagrafici","IdFiscaleIVA","IdPaese"].text,
+                               sede: Sede(indirizzo: p["Sede","Indirizzo"].text,
+                                          cap: p["Sede","CAP"].text,
+                                          comune: p["Sede","Comune"].text,
+                                          provincia: p["Sede","Provincia"].text,
+                                          nazione: p["Sede","Nazione"].text),
+                               cfiscale: p["DatiAnagrafici","CodiceFiscale"].text,
+                               nome: p["DatiAnagrafici","IdFiscaleIVA","IdPaese"].text)
+        }
+        return builder.build()
+    }
+
 }
---
+++ fattura/models.swift
@@ -0,0 +1,68 @@
+//
+//  models.swift
+//  fattura
+//
+//  Created by Alberto Negri on 27/7/20.
+//  Copyright © 2020 calber. All rights reserved.
+//
+
+import Foundation
+import XMLTools
+
+struct Fattura {
+    var committente = DatiAnagrafici(iva: IdFiscaleIVA()) //CessionarioCommittente(anagrafica: DatiAnagrafici(iva: IdFiscaleIVA()))
+    var prestatore = DatiAnagrafici(iva: IdFiscaleIVA(), sede: Sede())
+}
+
+
+struct Sede {
+    var indirizzo: String = ""
+    var cap: String = ""
+    var comune: String = ""
+    var provincia: String = ""
+    var nazione: String = ""
+}
+
+struct Anagrafica {
+    var denominazione: String
+}
+
+struct DatiAnagrafici {
+    var iva: IdFiscaleIVA
+    var codiceFiscale: String = ""
+    var anagrafica: Anagrafica = Anagrafica(denominazione: "")
+    var sede: Sede = Sede()
+}
+
+struct IdFiscaleIVA {
+    var IdPaese: String = ""
+    var IdCodice: String = ""
+}
+
+class FatturaBuilder {
+    private var fattura: Fattura
+    
+    init() {
+        fattura = Fattura()
+    }
+    
+    func committente(iva: String, paese: String, nome: String) -> FatturaBuilder {
+        fattura.committente.iva.IdCodice = iva
+        fattura.committente.iva.IdPaese = paese
+        fattura.committente.anagrafica.denominazione = nome
+        return self
+    }
+    
+    func prestatore(iva: String, paese: String, sede: Sede, cfiscale: String, nome: String) -> FatturaBuilder {
+        fattura.prestatore.iva.IdCodice = iva
+        fattura.prestatore.iva.IdPaese = paese
+        fattura.prestatore.codiceFiscale = cfiscale
+        fattura.prestatore.anagrafica.denominazione = nome
+        fattura.prestatore.sede = sede
+        return self
+    }
+
+    func build() -> Fattura {
+        return self.fattura
+    }
+}
